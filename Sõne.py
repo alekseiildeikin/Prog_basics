16948aae0bd47d6c148bdb25762770117f2fafe4
alekseiildeikin <aleksei.ildeikin@voco.ee>
2025-10-03 11:20:46 +0300

    Done

---
+++ Sõne.py
@@ -0,0 +1,10 @@
+first_name = "James"
+last_name = "Bond"
+full_name = f"{first_name} {last_name}"
+self_description_sentence = f"My name is {last_name}, {first_name} {last_name}."
+cake = "vahukoormarjadtäidispõhi"
+print(cake[:8] + "\n" + cake[8:14] + "\n" + cake[14:20] + "\n" + cake[20:])
+original_string = "Programming is fun!"
+backwards = "".join(reversed(original_string))
+every_other = original_string[::2]
+first_word_reversed = "".join(reversed(original_string.split()[0]))
